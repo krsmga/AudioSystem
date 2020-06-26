7f812d0316a8b0be34cfd8a75f69f3558c257367
Kleber Ribeiro da Silva <krsmga@gmail.com>
2020-06-26 17:51:09 -0300

    Implementation of the _saveSettings variable
    You can now use the _saveSettings variable to enable the inspector to save the last audio settings before exiting the application.

--- AudioSystem.cs
+++ AudioSystem.cs
@@ -2,9 +2,8 @@
  * @author Kleber Ribeiro da Silva
  * @email krsmga@gmail.com
  * @create date 2020-06-23 15:13:45
- * @modify date 2020-06-23 15:13:45
+ * @modify date 2020-06-26 17:42:57
  * @desc This class controls the general Audio (AudioListener) of the current scene.
- * @github https://github.com/krsmga/AudioSystem
  */
 
 using System.Collections;
@@ -20,6 +19,7 @@ using UnityEngine.UI;
 /// <param name="_iconDisabledAudio">Attach a GameObject that contains an Image (Sprite) to represent the disabled audio. (Optional)</param>
 /// <param name="_iconVolume">Attach images (Sprites) with a sequence that demonstrates the volume level. From the smallest to the largest. (Optional)</param>
 /// <param name="_audioListener">Attach the AudioListener component of the current scene.</param>
+/// <param name="_saveSettings">If true, allows you to save the latest audio settings before closing the application.</param>
 /// <param name="_volume">Sets the volume value of the General Audio (AudioListener).</param>
 /// </remarks>
 public class AudioSystem : MonoBehaviour
@@ -29,6 +29,7 @@ public class AudioSystem : MonoBehaviour
     [SerializeField] private GameObject _iconDisabledAudio = default;
     [SerializeField] private Sprite[] _iconVolume = default;
     [SerializeField] private AudioListener _audioListener = default;
+    [SerializeField] private bool _saveSettings = default;
 
     [Range(0.0f, 1.0f)]
     [SerializeField] private float _volume = default;
@@ -47,11 +48,8 @@ public class AudioSystem : MonoBehaviour
         { return _isActive; }
         set 
         {
-            if (_isActive != value)
-            {
-                _isActive = value;
-                UpdateIcons(_isActive, -1);
-            }
+            _isActive = value;
+            UpdateIcons(_isActive, -1);
         }
     }
 
@@ -96,11 +94,24 @@ public class AudioSystem : MonoBehaviour
     // Start default values
     void Awake() 
     {
+        if (_saveSettings)
+        {
+            if (PlayerPrefs.HasKey("enableAudioApp"))
+            {
+                isActive = PlayerPrefs.GetInt("enableAudioApp") == 1;
+            }
+
+            if (PlayerPrefs.HasKey("volumeAudioApp"))
+            {
+                _volume = PlayerPrefs.GetFloat("volumeAudioApp");
+            }
+        }
+
         AudioListener.volume = _volume;
         volume = _volume;
     }
 
-    private void UpdateIcons(bool value, int volume) 
+    private void UpdateIcons(bool value, int volumeIndex) 
     {
         if (_audioListener != null)
         {
@@ -110,18 +121,39 @@ public class AudioSystem : MonoBehaviour
         if (_iconEnabledAudio != null)
         {
             _iconEnabledAudio.SetActive(value);
-            if (_iconVolume.Length > 0 && volume >= 0)
+            if (_iconVolume.Length > 0 && volumeIndex >= 0)
             {
-                _iconEnabledAudio.GetComponent<Image>().sprite = _iconVolume[volume];
+                _iconEnabledAudio.GetComponent<Image>().sprite = _iconVolume[volumeIndex];
             }
         }
 
         if (_iconDisabledAudio != null)
         {
             _iconDisabledAudio.SetActive(!value);
-            if (_iconVolume.Length > 0 && volume >= 0)
+            if (_iconVolume.Length > 0 && volumeIndex >= 0)
             {
-                _iconDisabledAudio.GetComponent<Image>().sprite = _iconVolume[volume];
+                _iconDisabledAudio.GetComponent<Image>().sprite = _iconVolume[volumeIndex];
+            }
+        }
+    }
+
+    void OnDestroy() 
+    {
+        if (_saveSettings)
+        {
+            PlayerPrefs.SetFloat("volumeAudioApp", volume);
+            PlayerPrefs.SetInt("enableAudioApp",isActive ? 1 : 0);
+        }
+        else
+        {
+            if (PlayerPrefs.HasKey("enableAudioApp"))
+            {
+                PlayerPrefs.DeleteKey("enableAudioApp");
+            }
+
+            if (PlayerPrefs.HasKey("volumeAudioApp"))
+            {
+                PlayerPrefs.DeleteKey("volumeAudioApp");
             }
         }
     }
